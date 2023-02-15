adf750e4eb413a236cbaec206bb7e65bdab915df
mohamedmehdigara <garamehdi57@yahoo.com>
2023-02-15 12:56:59 +0100

    added files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import { Component } from 'react';
+import Post from './components/Post';
+import Header from "./components/Header"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+
+    return (
+
+      <div>
+
+        <Header />
+
+        <div>
+
+          <Post />
+
+        </div>
+
+      </div>
+
+    );
+
+  }
+
 }
 
 export default App;
